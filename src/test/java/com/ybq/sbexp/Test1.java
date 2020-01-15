e6419f5dac9872b20cbfaa6f6a0bb9583ceac43b
nickName <876613910@qq.com>
2020-01-15 15:20:50 +0800

    collection.vsdx

--- src/test/java/com/ybq/sbexp/Test1.java
+++ src/test/java/com/ybq/sbexp/Test1.java
@@ -1,13 +1,26 @@
 package com.ybq.sbexp;
 
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class Test1 {
 
     public static void main(String[] args) {
-        String[] s = new String[] {"a", "b"};
+        TreeMap<String, String> map = new TreeMap<>();
+        map.put("b", "b");
+        map.put("a", "a");
+        System.out.println(map);
+
+        TreeSet<String> set = new TreeSet<>();
+        set.add("b");
+        set.add("a");
+        System.out.println(set);
+        String[] s = {"a", "b"};
+        char[] c = {'a', 'b'};
+        int[] i = {2, 1};
         System.out.println(s);
-        char[] b = {'a', 'b'};
-        System.out.println(b);
-        b[0] = 'c';
-        System.out.println(b);
+        System.out.println(c);
+        System.out.println(i);
+
     }
 }
